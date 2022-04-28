24ee30e311d941a5d64451dad4a8d41de18fd6cf
Faizan Ahmed <43652410+faizancodes@users.noreply.github.com>
2022-04-27 23:43:11 -0400

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,8 @@
+twitter_access_token = ""
+twitter_access_token_secret = ""
+twitter_API_key = ""
+twitter_API_secret_key = ""
+
+spotify_username = ''
+spotify_client_id = ''
+spotify_client_secret = ''
